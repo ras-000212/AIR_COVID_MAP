dc057bb8292899bfa3f68a227bd1da826652c330
Alexis <>
2020-03-27 12:17:29 +0100

    parsing data

--- covid-19.js
+++ covid-19.js
@@ -1,27 +1,32 @@
 window.addEventListener("DOMContentLoaded",main);
 
-var COVID_API = `https://api.covid19api.com/`
+var COVID_API = `https://api.covid19api.com/`;
 
 
 
+function call(endpoint, requestOptions){
+  return fetch(`${endpoint}`, requestOptions);
+}
 //Get casses of a country depends of the status(confirmed,recovered,deaths)
 function get_country_status(country,status){
-  COVID_OPTION = `country/${country}/status/${status}`;
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  console.log(`${COVID_API}${COVID_OPTION}`)
-  fetch(`${COVID_API}${COVID_OPTION}`, requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log(`error`, error));
+  var COVID_OPTION = `country/${country}/status/${status}`;
+  var p = new Promise(function(resolve,reject){
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    call(`${COVID_API}${COVID_OPTION}`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log(`error`, error));
+  })
 }
+  
+
 
 function main(){
     var button = document.getElementById("b-covid");
-    button.addEventListener(`click`, function(){
-        window.alert(`get_country_status`);
+    button.addEventListener(`click`, function(){  
         get_country_status(`france`,`confirmed`);
     })
 } 
