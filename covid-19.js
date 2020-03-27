2c729c00b77469877742853cfb5cc4696ca8e596
Alexis <>
2020-03-27 09:25:05 +0100

    res dans console du navigateur

---
+++ covid-19.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded",main);
+
+function main(){
+    var button = document.getElementById("b");
+    button.addEventListener('click', function(){
+        window.alert("teeeeeeeeeeest");
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+          
+          fetch("https://api.covid19api.com/", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    });
+}
