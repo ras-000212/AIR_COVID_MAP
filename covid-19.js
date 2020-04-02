23202a4a33f3a6ddc4a5cef8e0451fec96f99283
Alexis <>
2020-04-02 19:17:14 +0200

    return countries

--- covid-19.js
+++ covid-19.js
@@ -16,7 +16,9 @@ export function get_country_status(country,status){
 }
 
 
+//use this for the drowpdown list
 export function getCountries(){
+  let countries=[];
   var requestOptions = {
     method: 'GET',
     redirect: 'follow'
@@ -24,7 +26,14 @@ export function getCountries(){
   
   fetch("https://api.covid19api.com/countries", requestOptions)
     .then(response => response.json())
-    .then(result => console.log(result))
+    .then(result => {
+      console.log(result);
+      for(var i = 1; i <result.length;i++){
+        countries[i]=result[i].Country;
+        console.log(countries[i]);
+      }
+      return countries;
+    })
     .catch(error => console.log('error', error));
 } 
 
