a5d4494429d87df276043cc8d368ebe98f42b8c9
Gaëlle Ferreira <gaelle_f@live.fr>
2020-04-02 15:56:35 +0200

    pb air quality

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var covid = require(`./covid-19.js`);
+var air = require(`./air-quality.js`);
 
 function main(){
 	var button = document.getElementById("b-covid");
@@ -8,8 +9,9 @@ function main(){
 
 	var button = document.getElementById("b-air");
 	button.addEventListener('click', async function(){
-		var state = await get_states_list()
-		await get_cities_list(state)
+		var state;
+		state=await air.get_states_list(state);
+		//await get_cities_list(state)
 	});
 }
 
